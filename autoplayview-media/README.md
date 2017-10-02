fecae6af76d936c7d41269da3fb2193ffb344a34
Manuel Sommerhalder <manuel.sommerhalder@goldinteractive.ch>
2017-10-02 09:57:11 +0200

    Add Vimeo video controller

--- autoplayview-media/README.md
+++ autoplayview-media/README.md
@@ -1,6 +1,6 @@
 ## AutoPlayView Media
 
-Auto play media (video/sound) when in viewport. Currently just supporting youtube videos.
+Auto play media (video/sound) when in viewport. Currently just supporting youtube and vimeo videos.
 
 ### Dependencies
 
@@ -8,8 +8,17 @@ Auto play media (video/sound) when in viewport. Currently just supporting youtub
 
 ### Markup example
 
-Youtube Video:
+Youtube Video (API will be loaded automatically):
 
 ```html
-<iframe data-feature="autoplayview-media" data-type="youtube" width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1" frameborder="0" allowfullscreen></iframe>
+<iframe data-feature="autoplayview-media" data-type="youtube" scrolling="no" frameborder="0" width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1"  allowfullscreen></iframe>
+```
+
+Vimeo Video:
+
+```html
+<iframe data-feature="autoplayview-media" data-type="vimeo" scrolling="no" frameborder="0" width="560" height="315" src="https://player.vimeo.com/video/78329678" allowfullscreen></iframe>
+
+<!-- Vimeo API -->
+<script src="https://player.vimeo.com/api/player.js"></script>
 ```
--- autoplayview-media/autoplayview-media.js
+++ autoplayview-media/autoplayview-media.js
@@ -1,6 +1,13 @@
 import ScrollMonitor from 'scrollmonitor'
 
+const API_YOUTUBE = 'https://www.youtube.com/iframe_api'
+
 const TYPE_YOUTUBE = 'youtube'
+const TYPE_VIMEO = 'vimeo'
+
+const ACTION_PLAY = 'play'
+const ACTION_PAUSE = 'pause'
+const ACTION_STOP = 'stop'
 
 /**
  * Auto play media when in viewport.
@@ -16,8 +23,11 @@ class AutoPlayViewMedia extends gi.features.Feature {
       case TYPE_YOUTUBE:
         this.controller = new YoutubeController(this.node)
         break
+      case TYPE_VIMEO:
+        this.controller = new VimeoController(this.node)
+        break
       default:
-        this.controller = new Controller(this.node)
+        throw new Error(`Media type "${mediaType}" not defined`)
     }
 
     this.watcher = ScrollMonitor.create(this.node)
@@ -48,10 +58,6 @@ class AutoPlayViewMedia extends gi.features.Feature {
 
 }
 
-
-const ACTION_PLAY = 'play'
-const ACTION_PAUSE = 'pause'
-const ACTION_STOP = 'stop'
 
 class Controller {
 
@@ -86,14 +92,51 @@ class Controller {
 
 }
 
+
+class VimeoController extends Controller {
+
+  init() {
+    this._loadPlayer()
+  }
+
+  playMedia() {
+    if (this.loaded) {
+      this.player.play()
+    }
+  }
+
+  pauseMedia() {
+    if (this.loaded) {
+      this.player.pause()
+    }
+  }
+
+  stopMedia() {
+    if (this.loaded) {
+      this.player.stop()
+    }
+  }
+
+  _loadPlayer() {
+    this.player = new Vimeo.Player(this.$element)
+    this.loaded = true
+  }
+
+}
+
+
 class YoutubeController extends Controller {
 
   init() {
-    this.apiReadyListener = this._onApiReady()
-    // listen to global youtube api event hub load event
-    gi.eventHub.on('apiReady:youtube', this.apiReadyListener)
-    // load youtube api library
-    gi.utils.fetch.script('https://www.youtube.com/iframe_api')
+    if (gi.utils.media.youtubeApiLoaded) {
+      this._loadPlayer()
+    } else {
+      this.apiReadyListener = this._onApiReady()
+      // listen to global youtube api event hub load event
+      gi.eventHub.on('apiReady:youtube', this.apiReadyListener)
+      // load youtube api library
+      gi.utils.fetch.script(API_YOUTUBE)
+    }
   }
 
   destroy() {
@@ -118,26 +161,30 @@ class YoutubeController extends Controller {
     }
   }
 
-  _onApiReady() {
-    return () => {
-      this.player = new YT.Player(this.$element, {
-        events: {
-          onStateChange: (e) => {
-            if (e.data == YT.PlayerState.ENDED) {
-              this.ended = true
-            } else {
-              this.ended = false
-            }
-          },
-          onReady: () => {
-            this.loaded = true
+  _loadPlayer() {
+    this.player = new YT.Player(this.$element, {
+      events: {
+        onStateChange: (e) => {
+          if (e.data == YT.PlayerState.ENDED) {
+            this.ended = true
+          } else {
+            this.ended = false
+          }
+        },
+        onReady: () => {
+          this.loaded = true
 
-            if (this.actions[this.actions.length - 1] == ACTION_PLAY) {
-              this.play()
-            }
+          if (this.actions[this.actions.length - 1] == ACTION_PLAY) {
+            this.play()
           }
         }
-      })
+      }
+    })
+  }
+
+  _onApiReady() {
+    return () => {
+      this._loadPlayer()
     }
   }
 
